be7132809d758d081e5f328c44ae23285eb63e00
jgmartinss <jgmartinsss@hotmail.com>
2017-10-23 14:07:30 -0200

    update sql setup

--- app/code/local/Trezo/Correios/sql/trezo_correios_setup/install-0.1.0.php
+++ app/code/local/Trezo/Correios/sql/trezo_correios_setup/install-0.1.0.php
@@ -14,3 +14,61 @@
  
 
  /** @var $installer Mage_Core_Model_Resource_Setup */
+
+$installer = $this;
+
+$installer->startSetup();
+
+
+/* @var $installer Mage_Catalog_Model_Resource_Eav_Mysql4_Setup */
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
+// Add atribute Length
+
+$codOne = "legth";
+
+$configOne = array(
+	'position' => 1,
+	'required' => 0,
+	'label'    => 'Comprimento',
+	'type'     => 'int',
+	'input'    => 'text',
+	'apply_to' => 'simple,bundle,grouped,configurable',
+	'note'     => 'Comprimento mínimo 16(cm)'
+);
+
+$setup->addAttribute('catalog_product', $codOne, $configOne);
+
+// Add atribute Width
+
+$codTwo = "height";
+
+$configTwo = array(
+	'position' => 1,
+	'required' => 0,
+	'label'    => 'Altura',
+	'type'     => 'int',
+	'input'    => 'text',
+	'apply_to' => 'simple,bundle,grouped,configurable',
+	'note'     => 'Altura mínimo 2(cm)'
+);
+
+$setup->addAttribute('catalog_product', $codTwo, $configTwo);
+
+// Add atribute Height
+
+$codThree = "width";
+
+$configThree = array(
+	'position' => 1,
+	'required' => 0,
+	'label'    => 'Largura',
+	'type'     => 'int',
+	'input'    => 'text',
+	'apply_to' => 'simple,bundle,grouped,configurable',
+	'note'     => 'Largura mínimo 11(cm)'
+);
+
+$setup->addAttribute('catalog_product', $codThree, $configThree);
+
+$installer->endSetup();
